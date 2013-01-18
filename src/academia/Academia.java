f48cb3edb205e4a2eed7491d7c11c0301175f220
Sergio Blasco <sblasco73-github@yahoo.es>
2013-01-18 13:39:22 +0100

    merge Academia

--- src/academia/Academia.java
+++ src/academia/Academia.java
@@ -1,13 +1,45 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package academia;
 
-/**
- *
- * @author Usuario
- */
 public class Academia {
+
+    String nombre;
+    String direccion;
     
+    Map<Integer, Alumno> mapAlumnos;
+    Map<Integer, Profesor> mapProfesores;
+    Map<Integer, Asignatura> mapAsignaturas;
+    Map<String, Aulas> mapAulas;
+    
+    public void addAlumno(Alumno alumno) {
+        
+    }
+    
+    public void removeAlumno(Integer idAlumno) {
+        
+    }
+    
+    public void addProfesor(Alumno alumno) {
+        
+    }
+    
+    public void removeProfesor(Integer idAlumno) {
+        
+    }
+    
+    public void addAsignatura(Alumno alumno) {
+        
+    }
+    
+    public void removeAsignatura(Integer idAlumno) {
+        
+    }
+    
+    public void addAula(Alumno alumno) {
+        
+    }
+    
+    public void removeAula(Integer idAlumno) {
+        
+    }
+>>>>>>> SBR Clase Academia
 }
