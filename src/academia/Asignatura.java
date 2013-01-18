08acec2552ab7d9e24fe158a42946a75267002e0
JM Barea <jm.barea.remiro@gmail.com>
2013-01-18 13:57:05 +0100

    updating methods

--- src/academia/Asignatura.java
+++ src/academia/Asignatura.java
@@ -4,14 +4,46 @@ import java.util.Date;
 import java.util.Set;
 
 public class Asignatura {
-    Integer id;
-    String nombre;
-    Profesor profesor;
-    Aula aula;
-    int horas;
-    Date inicio;
-    Date fin;
-    Set<Alumno> alumnos;
+    private Integer id;
+    private String nombre;
+    private Profesor profesor;
+    private Aula aula;
+    private int horas;
+    private Date inicio;
+    private Date fin;
+    private Set<Alumno> alumnos;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public Aula getAula() {
+        return aula;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public Date getInicio() {
+        return inicio;
+    }
+
+    public Date getFin() {
+        return fin;
+    }
+
+    public Set<Alumno> getAlumnos() {
+        return alumnos;
+    }
     
     public void addAlumno(Alumno alumno){
         
@@ -26,6 +58,26 @@ public class Asignatura {
 
     public void setAula(Aula aula) {
         this.aula = aula;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public void setInicio(Date inicio) {
+        this.inicio = inicio;
+    }
+
+    public void setFin(Date fin) {
+        this.fin = fin;
     }
     
     
